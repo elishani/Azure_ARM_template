1e52c7d42b5fb3c054fd08d3109c7eb2465ffbcb
ELi Shani <elishani50@gmail.com>
2020-09-06 14:04:55 +0300

    Update install-apache.sh

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -4,8 +4,8 @@ apt-get -y update
 
 # install Apache2
 apt-get -y install apache2
-apt-get -y install mysql-server
-apt-get -y install php libapache2-mod-php php-mysql
+#apt-get -y install mysql-server
+#apt-get -y install php libapache2-mod-php php-mysql
 # write some HTML
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 
