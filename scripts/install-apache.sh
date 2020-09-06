874d0a7db84fe34f5de925670845443481c6f902
ELi Shani <elishani50@gmail.com>
2020-09-06 23:39:32 +0300

    Update install-apache.sh

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -5,6 +5,7 @@ apt install -y apache2
 cat > /var/www/html/index.html <<EOF
 <h1> HELLO </h1>
 EOF
+systemctl restart apache2
 #echo '<h1> HELLO </h1>' > /var/www/html/index.html
 #apt update
 #apt install -y apache2
