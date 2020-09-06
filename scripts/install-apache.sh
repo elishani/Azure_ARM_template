94a7cc1003cee1fd73e0e6c4e51745ed6967d4f9
ELi Shani <elishani50@gmail.com>
2020-09-06 19:11:23 +0300

    Update install-apache.sh

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt update
-apt install -y apache2
-echo "<h1> HELLO </h1>" > /var/www/html/index.html
-sysytemctl restart apache2
+apt install -y nginx
+#apt update
+#apt install -y apache2
+#echo "<h1> HELLO </h1>" > /var/www/html/index.html
+#sysytemctl restart apache2
