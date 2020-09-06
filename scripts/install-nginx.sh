a54cd9ff2463073aee3b7e119f343f4e2d149d07
ELi Shani <elishani50@gmail.com>
2020-09-07 01:22:12 +0300

    Update install-nginx.sh

--- scripts/install-nginx.sh
+++ scripts/install-nginx.sh
@@ -26,4 +26,4 @@ mkdir ssl
 chmod 700 ssl
 yes "" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/eli.key -out  /etc/nginx/ssl/eli.crt
 #nginx -t 
-#systemctl reload nginx
+systemctl reload nginx
