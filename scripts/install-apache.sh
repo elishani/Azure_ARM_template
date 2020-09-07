ab4b5fd2e08b75311f4abbbce1ab01eea400b856
ELi Shani <elishani50@gmail.com>
2020-09-07 15:16:17 +0300

    Update install-apache.sh

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sleep 60
-apt update
-apt install -y apache2
-cat > /var/www/html/index.html <<EOF
-<h1> HELLO </h1>
-EOF
-systemctl restart apache2
-#echo '<h1> HELLO </h1>' > /var/www/html/index.html
-#apt update
-#apt install -y apache2
-#echo "<h1> HELLO </h1>" > /var/www/html/index.html
-#sysytemctl restart apache2
+dpkg --configure -a
+apt-get -y update
+
+# install Apache2
+apt-get -y install apache2
+apt-get -y install mysql-server
+apt-get -y install php libapache2-mod-php php-mysql
+# write some HTML
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+# restart Apache
+apachectl restart
